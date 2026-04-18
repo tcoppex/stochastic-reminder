4cae7d46f0011e57825ffe520b35ae1662785c6b
Thibault Coppex <tcoppex@proton.me>
2026-04-18 13:48:26 +0200

    Remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,18 +68,8 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.work.runtime.ktx)
-    implementation(libs.androidx.junit.ktx)
     implementation(libs.androidx.core.splashscreen)
     ksp(libs.androidx.room.compiler)
-
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.runner)
-//    androidTestImplementation(libs.androidx.rules)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//    androidTestImplementation(platform(libs.androidx.compose.bom))
-//    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
