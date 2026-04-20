d36e26158dcfe0b1af81d608adbe7356136480f9
Thibault Coppex <tcoppex@proton.me>
2026-04-20 18:19:08 +0200

    Fix semantic version number.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 2
-        versionName = "1.1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
