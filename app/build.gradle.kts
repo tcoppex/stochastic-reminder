0bd0ea7e96c4f92648fa69678a8834fbc23fb96e
Thibault Coppex <tcoppex@proton.me>
2026-04-26 19:42:24 +0200

    Bump version to "1.2.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "cc.polysfaer.stochapop"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
